2745ec29c93ef0e6e4001a5c2b2c714604d2ad86
Julien Veyssier <eneiluj@posteo.net>
2018-12-04 04:01:22 +0100

    sync nosync design

--- app/src/main/java/it/eneiluj/nextcloud/phonetrack/service/WebTrackService.java
+++ app/src/main/java/it/eneiluj/nextcloud/phonetrack/service/WebTrackService.java
@@ -5,7 +5,6 @@ import android.app.IntentService;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.util.Log;
 
 import org.json.JSONException;
