60b95d267733ddf8961d52b6f6847a08cef6e002
Julien Veyssier <eneiluj@posteo.net>
2019-01-11 02:42:32 +0100

    remove splashscreen

--- app/src/main/java/net/eneiluj/nextcloud/phonetrack/android/activity/SplashscreenActivity.java
+++ app/src/main/java/net/eneiluj/nextcloud/phonetrack/android/activity/SplashscreenActivity.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by stefan on 18.04.17.
- */
+
 public class SplashscreenActivity extends AppCompatActivity {
 
     @Override
