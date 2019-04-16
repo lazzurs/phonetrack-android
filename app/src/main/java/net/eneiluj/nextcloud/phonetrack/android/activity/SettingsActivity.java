8998f8772574c30011f139bdc1faae37231a66ff
Julien Veyssier <eneiluj@posteo.net>
2019-04-16 02:09:20 +0200

    hide SSO switch on problematic android versions

--- app/src/main/java/net/eneiluj/nextcloud/phonetrack/android/activity/SettingsActivity.java
+++ app/src/main/java/net/eneiluj/nextcloud/phonetrack/android/activity/SettingsActivity.java
@@ -118,6 +118,11 @@ public class SettingsActivity extends AppCompatActivity {
         btn_submit = findViewById(R.id.settings_submit);
         urlWarnHttp = findViewById(R.id.settings_url_warn_http);
 
+        // disable SSO on Android versions where it crashes
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && Build.VERSION.SDK_INT <= Build.VERSION_CODES.N_MR1) {
+            use_sso_switch.setVisibility(View.GONE);
+        }
+
         //ButterKnife.bind(this);
 
         preferences = PreferenceManager
