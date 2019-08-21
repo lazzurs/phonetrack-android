43aa676a378611898a7435bcd4f6d1ea612a182d
Julien Veyssier <eneiluj@posteo.net>
2019-08-21 12:45:14 +0200

    refs #108 change duration format to hh:mm:ss

--- app/src/main/java/net/eneiluj/nextcloud/phonetrack/util/SupportUtil.java
+++ app/src/main/java/net/eneiluj/nextcloud/phonetrack/util/SupportUtil.java
@@ -142,17 +142,11 @@ public class SupportUtil {
                     (absSeconds % 3600) / 60,
                     absSeconds % 60);
         }
-        else if (absSeconds >= 3600) {
+        else {
             positive = String.format(
                     "%02d:%02d:%02d",
                     absSeconds / 3600,
                     (absSeconds % 3600) / 60,
-                    absSeconds % 60);
-        }
-        else {
-            positive = String.format(
-                    "%02d:%02d",
-                    absSeconds / 60,
                     absSeconds % 60);
         }
         return seconds < 0 ? "-" + positive : positive;
