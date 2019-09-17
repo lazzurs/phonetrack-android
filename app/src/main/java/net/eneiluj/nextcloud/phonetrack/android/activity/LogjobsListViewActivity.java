d700fd4d08c23b6c36c69c5217d424f5e105c49b
Julien Veyssier <eneiluj@posteo.net>
2019-09-17 19:40:07 +0200

    enable NC maps logjobs as it s been released!

--- app/src/main/java/net/eneiluj/nextcloud/phonetrack/android/activity/LogjobsListViewActivity.java
+++ app/src/main/java/net/eneiluj/nextcloud/phonetrack/android/activity/LogjobsListViewActivity.java
@@ -375,8 +375,6 @@ public class LogjobsListViewActivity extends AppCompatActivity implements ItemAd
                         fabCreateSession.setVisibility(View.GONE);
                         fabCreateMaps.setVisibility(View.GONE);
                     }
-                    // TODO remove this when Maps is released
-                    fabCreateMaps.setVisibility(View.GONE);
                 } else {
 
                 }
