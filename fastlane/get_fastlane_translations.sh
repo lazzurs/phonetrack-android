a655545b6f21492580b65a2a78c706322db8f39e
Julien Veyssier <eneiluj@posteo.net>
2020-01-26 23:22:50 +0100

    improve fastlane files generation
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- fastlane/get_fastlane_translations.sh
+++ fastlane/get_fastlane_translations.sh
@@ -1,48 +1,37 @@
 #!/bin/bash
 
-test -d metadata/android/fr-FR || mkdir metadata/android/fr-FR
-cp ../app/src/main/res/fr/phonetrack_android_fastlane/full_description.txt metadata/android/fr-FR/full_description.txt
-cp ../app/src/main/res/fr/phonetrack_android_fastlane/short_description.txt metadata/android/fr-FR/short_description.txt
-echo "PhoneTrack" > metadata/android/fr-FR/title.txt
+declare -A langCodes
 
-test -d metadata/android/de-DE || mkdir metadata/android/de-DE
-cp ../app/src/main/res/de/phonetrack_android_fastlane/full_description.txt metadata/android/de-DE/full_description.txt
-cp ../app/src/main/res/de/phonetrack_android_fastlane/short_description.txt metadata/android/de-DE/short_description.txt
-echo "PhoneTrack" > metadata/android/de-DE/title.txt
+langCodes=(
+    ["fr"]="fr-FR" ["de"]="de-DE" ["es"]="es-ES" ["el"]="el-GR" ["it"]="it-IT"
+    ["nl"]="nl-NL" ["pl"]="pl-PL" ["ro"]="ro-RO" ["ru"]="ru-RU"
+    ["vi"]="vi-VN" ["eo"]="eo-UY" ["pt-BR"]="pt-BR" ["sr"]="sr-SP"
+    ["tr"]="tr-TR" ["oc"]="oc-FR" ["fa"]="fa-IR" ["fi"]="fi-FI"
+    ["zh-CN"]="zh-CN" ["hu"]="hu-HU" ["ta"]="ta-IN" ["pt-PT"]="pt-PT"
+    ["ar"]="ar-SA" ["es-ES"]="es-ES" ["cs"]="cs-CZ" ["th"]="th-TH"
+    ["sv-SE"]="sv-SE" ["en-GB"]="en-GB" ["qu"]="qu-PE" ["hi"]="hi-IN"
+    ["ca"]="ca-ES" ["sk"]="sk-SK" ["ne-NP"]="ne-NP" ["mn"]="mn-MN"
+    ["no"]="no-NO" ["ko"]="ko-KR" ["te"]="te-IN" ["en-US"]="en-US"
+    ["sl"]="sl-SI" ["bn"]="bn-BD" ["zh-TW"]="zh-TW" ["bg"]="bg-BG"
+    ["af"]="af-ZA" ["uk"]="uk-UA" ["ja"]="ja-JP" ["da"]="da-DK"
+    ["he"]="he-IL" ["id"]="id-ID"
+)
 
-test -d metadata/android/es-ES || mkdir metadata/android/es-ES
-cp ../app/src/main/res/es-ES/phonetrack_android_fastlane/full_description.txt metadata/android/es-ES/full_description.txt
-cp ../app/src/main/res/es-ES/phonetrack_android_fastlane/short_description.txt metadata/android/es-ES/short_description.txt
-echo "PhoneTrack" > metadata/android/es-ES/title.txt
+for path in `find ../app/src/main/res -name "full_description.txt"`; do
+    inFolder=`dirname $path`
+    inLangFolder=`dirname $inFolder`
+    inLang=`basename $inLangFolder`
+    outLang=${langCodes[$inLang]}
+    outFolder=metadata/android/$outLang
 
-test -d metadata/android/el-GR || mkdir metadata/android/el-GR
-cp ../app/src/main/res/el/phonetrack_android_fastlane/full_description.txt metadata/android/el-GR/full_description.txt
-cp ../app/src/main/res/el/phonetrack_android_fastlane/short_description.txt metadata/android/el-GR/short_description.txt
-echo "PhoneTrack" > metadata/android/el-GR/title.txt
-
-test -d metadata/android/it-IT || mkdir metadata/android/it-IT
-cp ../app/src/main/res/it/phonetrack_android_fastlane/full_description.txt metadata/android/it-IT/full_description.txt
-cp ../app/src/main/res/it/phonetrack_android_fastlane/short_description.txt metadata/android/it-IT/short_description.txt
-echo "PhoneTrack" > metadata/android/it-IT/title.txt
-
-test -d metadata/android/nl-NL || mkdir metadata/android/nl-NL
-cp ../app/src/main/res/nl/phonetrack_android_fastlane/full_description.txt metadata/android/nl-NL/full_description.txt
-cp ../app/src/main/res/nl/phonetrack_android_fastlane/short_description.txt metadata/android/nl-NL/short_description.txt
-echo "PhoneTrack" > metadata/android/nl-NL/title.txt
-
-test -d metadata/android/pl-PL || mkdir metadata/android/pl-PL
-cp ../app/src/main/res/pl/phonetrack_android_fastlane/full_description.txt metadata/android/pl-PL/full_description.txt
-cp ../app/src/main/res/pl/phonetrack_android_fastlane/short_description.txt metadata/android/pl-PL/short_description.txt
-echo "PhoneTrack" > metadata/android/pl-PL/title.txt
-
-test -d metadata/android/ro-RO || mkdir metadata/android/ro-RO
-cp ../app/src/main/res/ro/phonetrack_android_fastlane/full_description.txt metadata/android/ro-RO/full_description.txt
-cp ../app/src/main/res/ro/phonetrack_android_fastlane/short_description.txt metadata/android/ro-RO/short_description.txt
-echo "PhoneTrack" > metadata/android/ro-RO/title.txt
-
-test -d metadata/android/ru-RU || mkdir metadata/android/ru-RU
-cp ../app/src/main/res/ru/phonetrack_android_fastlane/full_description.txt metadata/android/ru-RU/full_description.txt
-cp ../app/src/main/res/ru/phonetrack_android_fastlane/short_description.txt metadata/android/ru-RU/short_description.txt
-echo "PhoneTrack" > metadata/android/ru-RU/title.txt
+    if [ ! -z "$outLang" ]; then
+        test -d $outFolder || mkdir $outFolder
+        cp $inFolder/full_description.txt $outFolder/
+        cp $inFolder/short_description.txt $outFolder/
+        echo 'PhoneTrack' > $outFolder/title.txt
+    else
+        echo "check $inLang language code!!!"
+    fi
+done
 
 git add .
