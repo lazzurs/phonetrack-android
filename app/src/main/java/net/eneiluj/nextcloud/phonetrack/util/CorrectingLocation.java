79c6198e1ac77472e447a716b499ac66066ea85a
Daniel Lublin <daniel@lublin.se>
2020-02-25 10:16:44 +0100

    squash! fixup! Handle timestamp from buggy GPS sensors (#150)
    Logic from https://github.com/nicolasvila/GPSRolloverCheck

--- app/src/main/java/net/eneiluj/nextcloud/phonetrack/util/CorrectingLocation.java
+++ app/src/main/java/net/eneiluj/nextcloud/phonetrack/util/CorrectingLocation.java
@@ -2,19 +2,26 @@ package net.eneiluj.nextcloud.phonetrack.util;
 
 import android.location.Location;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * Wraps the Location class to handle fix time from buggy sensors
  * that doesn't do correct GPS week number rollover.
  */
 public class CorrectingLocation extends Location {
+    private static final Date FIRST_ROLLOVER_DATE = new GregorianCalendar(1999, 8, 21).getTime();
+    private static final Date SECOND_ROLLOVER_DATE = new GregorianCalendar(2019, 4, 6).getTime();
+    private static final long ROLLOVER_CYCLE_IN_SECONDS = 1024*7*24*60*60;
     private long time;
 
     public CorrectingLocation(Location location) {
         super(location);
-        this.time = location.getTime();
+        time = location.getTime();
         // Add 1024 weeks of seconds if timestamp is off
-        if (this.time < 1_000_000_000) {
-            this.time += 1024*7*24*60*60;
+        if ((time > FIRST_ROLLOVER_DATE.getTime())
+                && (time < SECOND_ROLLOVER_DATE.getTime())) {
+            time += ROLLOVER_CYCLE_IN_SECONDS;
         }
     }
 
