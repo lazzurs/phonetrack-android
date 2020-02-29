41f7ee5ece00c69c66c5c52fa2ddb5e7909efe03
Julien Veyssier <eneiluj@posteo.net>
2020-02-29 20:01:45 +0100

    apply simple timestamp correction algorithm for the moment
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- app/src/main/java/net/eneiluj/nextcloud/phonetrack/util/CorrectingLocation.java
+++ app/src/main/java/net/eneiluj/nextcloud/phonetrack/util/CorrectingLocation.java
@@ -2,25 +2,20 @@ package net.eneiluj.nextcloud.phonetrack.util;
 
 import android.location.Location;
 
-import java.util.Date;
-import java.util.GregorianCalendar;
-
 /**
  * Wraps the Location class to handle fix time from buggy sensors
  * that doesn't do correct GPS week number rollover.
  */
 public class CorrectingLocation extends Location {
-    private static final Date FIRST_ROLLOVER_DATE = new GregorianCalendar(1999, 8, 21).getTime();
-    private static final Date SECOND_ROLLOVER_DATE = new GregorianCalendar(2019, 4, 6).getTime();
     private static final long ROLLOVER_CYCLE_IN_SECONDS = 1024*7*24*60*60;
     private long time;
 
     public CorrectingLocation(Location location) {
         super(location);
         time = location.getTime();
+        long now = System.currentTimeMillis() / 1000;
         // Add 1024 weeks of seconds if timestamp is off
-        if ((time > FIRST_ROLLOVER_DATE.getTime())
-                && (time < SECOND_ROLLOVER_DATE.getTime())) {
+        if (time < now - (ROLLOVER_CYCLE_IN_SECONDS / 2)) {
             time += ROLLOVER_CYCLE_IN_SECONDS;
         }
     }
